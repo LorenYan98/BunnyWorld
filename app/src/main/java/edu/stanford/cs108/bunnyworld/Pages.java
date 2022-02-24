fe9cb75db8339f38dfe961247ac08cbb6eb2be5d
Xiaokun <xiaokunchen0@gmail.com>
2022-02-23 22:33:38 -0800

    finish Pages

--- app/src/main/java/edu/stanford/cs108/bunnyworld/Pages.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/Pages.java
@@ -1,9 +1,45 @@
 package edu.stanford.cs108.bunnyworld;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Pages {
-    private String pageName;
-    private List<Shapes> shapeList = new ArrayList<>();
+    public static int pageCount = 1;
+    private final String pageName;
+    private final int boundary;
+    private final List<Shapes> shapeList;
+
+
+    Pages(int boundary, List<Shapes> shapeList) {
+        this.shapeList = shapeList;
+        this.boundary = boundary;
+        this.pageName = generateNextPageName();
+    }
+
+    private String generateNextPageName() {
+        String curPageName = "page " + pageCount;
+        pageCount++;
+        return curPageName;
+    }
+
+    public boolean isWithinPage(double y) {
+        return y <= boundary;
+    }
+
+    public void drawPage() {
+        for (int i = 0; i < shapeList.size(); i++) {
+            shapeList.get(i).draw();
+        }
+    }
+
+    public int getBoundary() {
+        return boundary;
+    }
+
+    public List<Shapes> getShapeList() {
+        return shapeList;
+    }
+
+    public String getPageName() {
+        return pageName;
+    }
 }
