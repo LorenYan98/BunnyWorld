0d1346d7caab0fd2b1cf2eae92dc63edebec2a9c
Xiaokun <xiaokunchen0@gmail.com>
2022-02-23 22:51:55 -0800

    finish Inventory

--- app/src/main/java/edu/stanford/cs108/bunnyworld/Inventory.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/Inventory.java
@@ -1,4 +1,31 @@
 package edu.stanford.cs108.bunnyworld;
 
+import java.util.List;
+
 public class Inventory {
+    private final int boundary;
+    private final List<Shapes> shapeList;
+
+    Inventory(int boundary, List<Shapes> shapeList) {
+        this.shapeList = shapeList;
+        this.boundary = boundary;
+    }
+
+    public boolean isWithinInventory(double y) {
+        return y > boundary;
+    }
+
+    public void drawInventory() {
+        for (int i = 0; i < shapeList.size(); i++) {
+            shapeList.get(i).draw();
+        }
+    }
+
+    public int getBoundary() {
+        return boundary;
+    }
+
+    public List<Shapes> getShapeList() {
+        return shapeList;
+    }
 }
