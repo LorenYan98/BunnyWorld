ffa6f44a1d5b3d53b8d7999fae4c9145f32b8ba3
Xiaokun <xiaokunchen0@gmail.com>
2022-03-03 16:38:04 -0800

    add EditorView display area, fix EditorView not extends View problem, construct EditorView

--- app/src/main/java/edu/stanford/cs108/bunnyworld/EditorActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/EditorActivity.java
@@ -3,6 +3,7 @@ package edu.stanford.cs108.bunnyworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    public void addPage(View view) {
+
     }
 }
--- app/src/main/java/edu/stanford/cs108/bunnyworld/EditorView.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/EditorView.java
@@ -1,5 +1,19 @@
 package edu.stanford.cs108.bunnyworld;
 
-public class EditorView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
 
+import androidx.annotation.Nullable;
+
+public class EditorView extends View {
+    public EditorView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
 }
