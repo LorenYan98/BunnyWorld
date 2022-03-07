ccb839b77ce31bcd1d66c42910ef10d540f13efd
Xiaokun <xiaokunchen0@gmail.com>
2022-03-06 19:55:43 -0800

    add on drop basic code

--- app/src/main/java/edu/stanford/cs108/bunnyworld/ScriptActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/ScriptActivity.java
@@ -29,7 +29,6 @@ public class ScriptActivity extends AppCompatActivity {
 
     private String onClickString;
     private String onEnterString;
-    private String onDropString;
 
     private String currentTrigger;
     private String currentAction;
@@ -73,13 +72,12 @@ public class ScriptActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_script);
         init();
-
+        init_on_drop();
     }
 
     public void init() {
         onClickString = "";
         onEnterString = "";
-        onDropString = "";
         updateSpinner();
 //        updateSpinner2();
     }
@@ -251,6 +249,211 @@ public class ScriptActivity extends AppCompatActivity {
     }
 
     public void concatenate() {
+        // fi shape exists, ignore currentPageSound
+        if (currentTrigger.equals(ON_CLICK)) {
+            if (onClickString.equals("")) {
+                if (currentShape != null && !currentShape.equals("DESELECT SHAPE")) {
+                    onClickString = currentTrigger + " " + currentAction + " " + currentShape;
+                } else {
+                    onClickString += " " + currentAction + " " + currentPageSound;
+                }
+            } else {
+                if (currentShape != null && !currentShape.equals("DESELECT SHAPE")) {
+                    onClickString += " " + currentAction + " " + currentShape;
+                } else {
+                    onClickString += " " + currentAction + " " + currentPageSound;
+                }
+            }
+        } else if (currentTrigger.equals(ON_ENTER)) {
+            if (onEnterString.equals("")) {
+                if (currentShape != null && !currentShape.equals("DESELECT SHAPE")) {
+                    onEnterString = currentTrigger + " " + currentAction + " " + currentShape;
+                } else {
+                    onEnterString += currentTrigger + " " + currentAction + " " + currentPageSound;
+                }
+            } else {
+                if (currentShape != null && !currentShape.equals("DESELECT SHAPE")) {
+                    onEnterString += " " + currentAction + " " + currentShape;
+                } else {
+                    onEnterString += " " + currentAction + " " + currentPageSound;
+                }
+            }
+        }
+    }
+
+    // VERY IMPORTANT!!!!! THE FOLLOWING CODE IS FOR ONDROP
+    private static final String ON_DROP = "onenter";
+    private String onDropString;
+
+//    private String currentTrigger_on_drop; no need
+    private String currentAction_on_drop;
+    private String currentPageSound_on_drop_1;
+    private String currentPageSound_on_drop_2;
+    private String currentShape_on_drop_1;
+    private String currentShape_on_drop_2;
+
+    private void init_on_drop() {
+        onDropString = "";
+        updateSpinner_on_drop();
+    }
+
+    private void updateSpinner_on_drop() {
+        Spinner triggerSpinner = (Spinner) findViewById(R.id.triggerSpinner);
+        Spinner actionsSpinner = (Spinner) findViewById(R.id.actionsSpinner);
+        Spinner pageSoundSpinner = (Spinner) findViewById(R.id.pageSoundSpinner);
+
+        // initiate ArrayList including all the actions
+        Map pageMap = EditorView.getPageMap();
+        Map soundMap = EditorView.getSoundMap();
+
+        List<String> pageKeyList = new ArrayList<String>(pageMap.keySet());
+        List<String> soundKeyList = new ArrayList<String>(soundMap.keySet());
+//        List<BPage> pageList = new ArrayList<>(pageMap.values());
+//        List<Bitmap> imgList = new ArrayList<>(imgMap.values());
+//        List<String> imgNameList = new ArrayList<>(imgMap.keySet());
+
+        // List<Sampler.Value> list = new ArrayList<Sampler.Value>(pageMap.values());
+
+
+        if (pageKeyList.contains(currentPageSound)) {
+            Spinner shapeSpinner = (Spinner) findViewById(R.id.shapeSpinner);
+            List shapeList = new ArrayList<String>(EditorView.getPageMap().get(currentPageSound).getShapeMap().keySet());
+            setCurrentShape((String) shapeList.get(0));
+        }
+
+
+        setCurrentTrigger(TRIGGERLIST.get(0));
+        setCurrentAction(ACTIONLIST.get(0));
+        setCurrentPageSound(pageKeyList.get(0));
+
+        ArrayAdapter<String> triggerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                TRIGGERLIST);
+
+        ArrayAdapter<String> actionAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                ACTIONLIST);
+
+        List<String> newList = new ArrayList<String>(pageKeyList);
+        newList.addAll(soundKeyList);
+
+        ArrayAdapter<String> pageAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                newList);
+
+        // Specify the layout to use when the list of choices appears
+        triggerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        actionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        triggerSpinner.setAdapter(triggerAdapter);
+        actionsSpinner.setAdapter(actionAdapter);
+        pageSoundSpinner.setAdapter(pageAdapter);
+
+        triggerSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String curItem = (String) adapterView.getSelectedItem();
+                setCurrentTrigger(curItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        actionsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String curItem = (String) adapterView.getSelectedItem();
+                setCurrentAction(curItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        pageSoundSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String curItem = (String) adapterView.getSelectedItem();
+                setCurrentPageSound(curItem);
+                updateSpinner2(pageKeyList, soundKeyList);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        updateSpinner2(pageKeyList, soundKeyList);
+    }
+
+    public void updateSpinner2(List pageKeyList, List soundKeyList) {
+        System.out.println("currpageKeyList: " + pageKeyList);
+        System.out.println("currentPageSound: " + currentPageSound);
+        if (pageKeyList.contains(currentPageSound)) {
+            Spinner shapeSpinner = (Spinner) findViewById(R.id.shapeSpinner);
+            List shapeList = new ArrayList<String>(EditorView.getPageMap().get(currentPageSound).getShapeMap().keySet());
+            if (!shapeList.isEmpty()) {
+                setCurrentShape((String) shapeList.get(0));
+                shapeList.add("DESELECT SHAPE");
+                for (int i = 0; i < shapeList.size(); i++) {
+                    System.out.println("curShape: " + shapeList.get(i).toString());
+                }
+                System.out.println("doneeeeeeeeeeeee");
+                ArrayAdapter<String> shapeAdapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_spinner_item,
+                        shapeList);
+                shapeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                shapeSpinner.setAdapter(shapeAdapter);
+
+                shapeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        String curItem = (String) adapterView.getSelectedItem();
+                        setCurrentShape(curItem);
+                        TextView imageName = findViewById(R.id.shapeImageTextView);
+                        if (currentShape != null && !currentShape.equals("DESELECT SHAPE")) {
+                            Map pageMap = EditorView.getPageMap();
+                            imageName.setText(EditorView.getPageMap().get(currentPageSound).getShapeMap().get(currentShape).getImageName());
+                        } else {
+                            imageName.setText("No shape is currently selected");
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            } else {
+                setCurrentShape(null);
+                shapeSpinner.setAdapter(null);
+                TextView imageName = findViewById(R.id.shapeImageTextView);
+                imageName.setText("No shape is currently selected");
+            }
+        } else {
+            setCurrentShape(null);
+            System.out.println("im   gere");
+            Spinner shapeSpinner = (Spinner) findViewById(R.id.shapeSpinner);
+            shapeSpinner.setAdapter(null);
+            TextView imageName = findViewById(R.id.shapeImageTextView);
+            imageName.setText("No shape is currently selected");
+        }
+    }
+
+    public void ADD_on_drop(View view) {
+        concatenate();
+        TextView combinedTextView = findViewById(R.id.combinedTextView);
+        combinedTextView.setText(onClickString + "\n" + onEnterString + "\n" +onDropString);
+    }
+
+    public void concatenate_on_drop() {
         // fi shape exists, ignore currentPageSound
         if (currentTrigger.equals(ON_CLICK)) {
             if (onClickString.equals("")) {
