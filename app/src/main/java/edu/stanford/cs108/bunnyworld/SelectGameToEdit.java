5e0037b3c384c8695f75ee29d47b53144b1a1694
gbb33vf <100183289+gbb33vf@users.noreply.github.com>
2022-03-08 22:33:30 -0800

    updated ui and implementing reading db for game names

--- app/src/main/java/edu/stanford/cs108/bunnyworld/SelectGameToEdit.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/SelectGameToEdit.java
@@ -3,15 +3,36 @@ package edu.stanford.cs108.bunnyworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectGameToEdit extends AppCompatActivity {
+
+    GameView gameView;
+    List<String> gameNames;
+    SingletonDB db;
+    String gameToLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_game_to_edit);
+
+        gameView = findViewById(R.id.game_view);
+        db = SingletonDB.getInstance(this);
+        gameNames = new ArrayList<>();
+        String queryStr = "SELECT game_name FROM games";
+        Cursor cursor = db.rawQuery(queryStr, null);
+        while (cursor.moveToNext()) {
+            gameNames.add(cursor.getString(0));
+        }
+        Log.d("gameNames", gameNames.toString());
+
     }
 
     public void goToNewEditor(View view) {
